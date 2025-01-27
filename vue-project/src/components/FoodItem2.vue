c73a7b2bd8a37476c2d0312655a1a1c40de00bdc
lsbernal <lsbernal@student.apc.edu.ph>
2025-01-27 09:47:04 +0800

    Added a second component

---
+++ vue-project/src/components/FoodItem2.vue
@@ -0,0 +1,27 @@
+<template>
+    <div>
+      <h2>{{ name }}</h2>
+      <p>{{ message }}</p>
+    </div>
+  </template>
+  
+  <script>
+  export default {
+    data() {
+      return {
+        name: 'Mangoes',
+        message: 'I like Mangoes'
+      }
+    }
+  };
+  </script>
+  
+  <style>
+  #app > div {
+  border: dashed black 1px;
+  display: inline-block;
+  margin: 10px;
+  padding: 10px;
+  background-color: lightyellow;
+}
+ </style>
