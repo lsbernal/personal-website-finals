96e0f5bbf933bc6ad8286c463062d0a6661e6856
lsbernal <lsbernal@student.apc.edu.ph>
2025-01-27 09:52:33 +0800

    updated

--- vue-project/src/App.vue
+++ vue-project/src/App.vue
@@ -3,9 +3,10 @@
 <template>
 
   <h1>Food</h1>
-  <food-item/>
-  <food-item2/>
-  <food-item/>
+  <food-item/> <br>
+  <food-item2/> <br>
+  <food-item/> <br>
+  <food-item2/> <br>
   
 </template>
 
