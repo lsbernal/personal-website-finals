ad030e909b87b178583acb37609329b18f52e3e5
lsbernal <lsbernal@student.apc.edu.ph>
2025-02-10 01:27:40 +0000

    updated comment and coutnry vue

--- vue-project/src/components/Comment.vue
+++ vue-project/src/components/Comment.vue
@@ -1,5 +1,5 @@
 <template>
-    <h1>Countries</h1>
+    <h1>Comments</h1>
       <ul>
           <li v-for="comment in comments" :key="comment.id">{{ comment.name }}</li>
       </ul>
@@ -17,13 +17,13 @@
     
     <script setup>
     import { ref, onMounted } from 'vue'
-    import { supabase } from './lib/supabaseClient'
+    import { supabase } from '../lib/supabaseClient'
     
     const comments = ref([])
     
     async function getComments() {
       const { data } = await supabase.from('comments').select()
-      countries.value = data
+      comments.value = data
     }
     
     onMounted(() => {
